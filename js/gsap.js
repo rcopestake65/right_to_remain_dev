cec2187b05b9a55ca73758c93524223a5ca8879a
rcopestake <richard@redink.co.uk>
2020-09-15 08:29:59 +0100

    home/map pages structure

--- js/gsap.js
+++ js/gsap.js
@@ -1,22 +1,3 @@
-/*gsap.to(".box", {
-    scrollTrigger: ".box", // start the animation when ".box" enters the viewport (once)
-    x: 500
-  });
-*/
-/*gsap.to(".box", {
-scrollTrigger: {
-    trigger: '.box',
-    start: "bottom bottom",
-    scrub: true
-},
-x: 500,
-rotation: 360,
-ease: "none",
-duration: 3
-
-});
-*/
-
 //fix navbar to top once it reaches it
 ScrollTrigger.create({
   trigger: ".nav-container",
