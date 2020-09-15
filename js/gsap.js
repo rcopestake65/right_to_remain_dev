d11144ff5d62ccddaf37806b37dceff9aa6e7f67
rcopestake <richard@redink.co.uk>
2020-09-15 07:36:29 +0100

    menu pinned with gsap - good

--- js/gsap.js
+++ js/gsap.js
@@ -3,7 +3,7 @@
     x: 500
   });
 */
-gsap.to(".box", {
+/*gsap.to(".box", {
 scrollTrigger: {
     trigger: '.box',
     start: "bottom bottom",
@@ -14,5 +14,21 @@ rotation: 360,
 ease: "none",
 duration: 3
 
-})
+});
+*/
+
+//fix navbar to top once it reaches it
+ScrollTrigger.create({
+  trigger: ".nav-container",
+  start: "top top", 
+  end: "+=5000",
+  pin: ".main-nav",
+  pinSpacing: false
+  //anticipatePin: 1, 
+  //markers: true
+});
+
+
+
+
 
