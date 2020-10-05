03c29e9de0470429d4b4c39fb8c92a397d44f49a
rcopestake <richard@redink.co.uk>
2020-10-05 12:18:38 +0100

    off screen menu test

--- js/sliding-menus.js
+++ js/sliding-menus.js
@@ -1,16 +1,20 @@
 //======main menu hamburger
 
 //open
-document.querySelector('.menu-open-btn').addEventListener('click', () => document.querySelector('.main-menu-container').classList.toggle('show'));
+document.querySelector('.menu-open-btn').addEventListener('click', () => document.querySelector('.main-nav').classList.toggle('show'));
+//rotate chevrons
+document.querySelector('.menu-open-btn').addEventListener('click', () => document.querySelector('.chev-right').classList.toggle('rotate'));
+document.querySelector('.menu-open-btn').addEventListener('click', () => document.querySelector('.chev-down').classList.toggle('rotate'));
+
 //close
-document.querySelector('.menu-close-btn').addEventListener('click', () => document.querySelector('.main-menu-container').classList.remove('show'));
+document.querySelector('.menu-close-btn').addEventListener('click', () => document.querySelector('.main-nav').classList.remove('show'));
 
 //close main-menu if problems btn clicked
-document.querySelector('.common-problems-btn').addEventListener('click', () => document.querySelector('.main-menu-container').classList.remove('show'));
+document.querySelector('.common-problems-btn').addEventListener('click', () => document.querySelector('.main-nav').classList.remove('show'));
 
 //close main-menu if star cards btn clicked
 const closeMenu2 = document.querySelectorAll(".card-btn");
 for (let i = 0; i < closeMenu2.length; i++) {
-    closeMenu2[i].addEventListener('click', () => document.querySelector('.main-menu-container').classList.remove('show'));
+    closeMenu2[i].addEventListener('click', () => document.querySelector('.main-nav').classList.remove('show'));
 }
 
