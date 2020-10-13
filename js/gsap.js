5b72215addd52109e94ec8c7431475c08648926f
rcopestake <richard@redink.co.uk>
2020-10-13 09:48:42 +0100

    end of dev-v3

--- js/gsap.js
+++ js/gsap.js
@@ -1,72 +1,82 @@
-
 // ============ HOME PAGE ===================
 
 gsap.set(".home img:nth-of-type(1)", { opacity: 0, y: -50 });
 gsap.to(".home img:nth-of-type(1)", { duration: 2, x: 0, y: 0, opacity: 1 });
 
 gsap.set(".home h2", { opacity: 0, y: -50, borderBottom: 0 });
-gsap.to(".home h2", { duration: 2, x: 0, y: 0, opacity: 1, delay: .3 });
+gsap.to(".home h2", { duration: 2, x: 0, y: 0, opacity: 1, delay: 0.3 });
 
 gsap.set(".underline", { width: 0 });
 gsap.to(".underline", { width: "45%", delay: 1.3, duration: 1 });
 
 gsap.set(".home img:nth-of-type(2)", { x: 150, opacity: 0 });
-gsap.to(".home img:nth-of-type(2)", { duration: 2, x: 0, y: 0, opacity: 1, delay: 1.1 });
+gsap.to(".home img:nth-of-type(2)", {duration: 2, x: 0, y: 0, opacity: 1, delay: 1.1,});
 
 gsap.set(".home p:nth-of-type(1)", { x: -150, opacity: 0 });
-gsap.to(".home p:nth-of-type(1)", { duration: 2, x: 0, y: 0, opacity: 1, delay: 1.1 });
+gsap.to(".home p:nth-of-type(1)", { duration: 2, x: 0, y: 0, opacity: 1, delay: 1.1,});
 
 gsap.set(".home p:nth-of-type(2)", { y: 50, opacity: 0 });
-gsap.to(".home p:nth-of-type(2)", { duration: 2, x: 0, y: 0, opacity: 1, delay: .5 });
+gsap.to(".home p:nth-of-type(2)", { duration: 2, x: 0, y: 0, opacity: 1, delay: 0.5,});
 
 gsap.set(".next-btn-container", { y: 50, opacity: 0 });
-gsap.to(".next-btn-container", { duration: 2, x: 0, y: 0, opacity: 1, delay: .7 });
+gsap.to(".next-btn-container", {  duration: 2, x: 0, y: 0, opacity: 1, delay: 0.7,});
 
-//more btn    
+//more btn
 gsap.to(".more-btn", { opacity: 1, duration: 1, repeat: -1, yoyo: true });
 
 //background shapes
-/*
-gsap.set(".shape-1", { opacity: 0 });
-gsap.to(".shape-1", { duration: 1, x: 0, y: 0, opacity: 1, scale: 1.2, transformOrigin: 'center', delay: .7 });
-
-gsap.set(".shape-2", {opacity: 0 });
-gsap.to(".shape-2", { duration: 2, x: 0, y: 0, opacity: 1, delay: 2 });
-*/
-var tlshape = gsap.timeline({ defaults: { duration: .8, scale: .2, ease: Back.easeOut.config(2), opacity: 0 } });
-tlshape.from(".shape-1", { delay: .5, transformOrigin: 'center' });
-tlshape.from(".shape-2", { delay: 0, transformOrigin: 'center' });
-tlshape.from(".shape-3", { delay: 0, transformOrigin: 'center' });
+const tlshape = gsap.timeline({
+  defaults: { duration: 0.8, scale: 0.2, ease: Back.easeOut.config(2), opacity: 0,},});
+tlshape.from(".shape-1", { delay: 0.5, transformOrigin: "center" });
+tlshape.from(".shape-2", { delay: 0, transformOrigin: "center" });
+tlshape.from(".shape-3", { delay: 0, transformOrigin: "center" });
 
 //=============== OVERVIEW & NAVIGATION =====================================================================
 
 gsap.set(".overview h3", { opacity: 0, y: -50 });
-gsap.to(".overview h3", { duration: 2, x: 0, y: 0, opacity: 1, delay: .3 });
+gsap.to(".overview h3", { duration: 2, x: 0, y: 0, opacity: 1, delay: 0.3 });
 
 gsap.set(".star-card", { x: 50, opacity: 0 });
-gsap.to(".star-card", { duration: 2, x: 0, y: 0, opacity: 1, delay: .3 });
+gsap.to(".star-card", { duration: 2, x: 0, y: 0, opacity: 1, delay: 0.3 });
 
-gsap.set([".map-item-1", ".map-item-3", ".map-item-5", ".map-item-7", ".map-item-9"], { x: -40, opacity: 0 });
-gsap.set([".map-item-2", ".map-item-4", ".map-item-6", ".map-item-8"], { x: 40, opacity: 0 });
+gsap.set(
+  [".map-item-1", ".map-item-3", ".map-item-5", ".map-item-7", ".map-item-9"],
+  { x: -40, opacity: 0 }
+);
+gsap.set([".map-item-2", ".map-item-4", ".map-item-6", ".map-item-8"], {
+  x: 40,
+  opacity: 0,
+});
 
-gsap.to([".map-item-1", ".map-item-2", ".map-item-3", ".map-item-4",
-  ".map-item-5", ".map-item-6", ".map-item-7", ".map-item-8", ".map-item-9"], { duration: 1, x: 0, opacity: 1, ease: "power.inOut(1, 0.3)", autoAlpha: 1, });
+gsap.to(
+  [
+    ".map-item-1",
+    ".map-item-2",
+    ".map-item-3",
+    ".map-item-4",
+    ".map-item-5",
+    ".map-item-6",
+    ".map-item-7",
+    ".map-item-8",
+    ".map-item-9",
+  ],
+  { duration: 1, x: 0, opacity: 1, ease: "power.inOut(1, 0.3)", autoAlpha: 1 }
+);
 
-gsap.to(".map-item-2", { delay: .2, });
-gsap.to(".map-item-3", { delay: .3, });
-gsap.to(".map-item-4", { delay: .4, });
-gsap.to(".map-item-5", { delay: .5, });
-gsap.to(".map-item-6", { delay: .6, });
-gsap.to(".map-item-7", { delay: .7, });
-gsap.to(".map-item-8", { delay: .8, });
+gsap.to(".map-item-2", { delay: 0.2 });
+gsap.to(".map-item-3", { delay: 0.3 });
+gsap.to(".map-item-4", { delay: 0.4 });
+gsap.to(".map-item-5", { delay: 0.5 });
+gsap.to(".map-item-6", { delay: 0.6 });
+gsap.to(".map-item-7", { delay: 0.7 });
+gsap.to(".map-item-8", { delay: 0.8 });
 
 //tab slide out btn
-gsap.set(".arrow-right", { opacity: .5 });
+gsap.set(".arrow-right", { opacity: 0.5 });
 gsap.to(".arrow-right", { opacity: 1, duration: 1, repeat: -1, yoyo: true });
 //tab slide down btn mobile
-gsap.set(".arrow-down", { opacity: .5 });
+gsap.set(".arrow-down", { opacity: 0.5 });
 gsap.to(".arrow-down", { opacity: 1, duration: 1, repeat: -1, yoyo: true });
-
 
 //mouseover form map items
 
@@ -77,8 +87,9 @@ function over() {
 }
 
 function out() {
-  gsap.to(this, 0.2, { y: 0, x: 0 })
-};
+  gsap.to(this, 0.2, { y: 0, x: 0 });
+}
+
 //mouseover home image/icon
 
 $(".home-btn").hover(overHome, outHome);
@@ -88,51 +99,56 @@ function overHome() {
 }
 
 function outHome() {
-  gsap.to(this, 0, { scaleX: 1, scaleY: 1 })
+  gsap.to(this, 0, { scaleX: 1, scaleY: 1 });
 }
-//mouseover star card 
-gsap.set(".star-card", { transformOrigin: '100% 100%' });
+//mouseover star card
+gsap.set(".star-card", { transformOrigin: "100% 100%" });
 $(".star-card").hover(starOver, starOut);
 
 function starOver() {
-  gsap.to(this, .2, { scaleX: '1.05', scaleY: '1.05', transformOrigin: "center center" });
+  gsap.to(this, 0.2, {
+    scaleX: "1.05",
+    scaleY: "1.05",
+    transformOrigin: "center center",
+  });
 }
 
 function starOut() {
-  gsap.to(this, .2, { scaleX: '1', scaleY: '1' });
+  gsap.to(this, 0.2, { scaleX: "1", scaleY: "1" });
 }
 //illustrations
 gsap.set(".illustration-1", { opacity: 0, x: -150 });
-gsap.to(".illustration-1", { duration: 2, x: 0, y: 0, opacity: 1, delay: .3 });
+gsap.to(".illustration-1", { duration: 2, x: 0, y: 0, opacity: 1, delay: 0.3 });
 
 gsap.set(".illustration-2", { opacity: 0, x: 150 });
-gsap.to(".illustration-2", { duration: 2, x: 50, y: 0, opacity: 1, delay: .3 });
+gsap.to(".illustration-2", {
+  duration: 2,
+  x: 50,
+  y: 0,
+  opacity: 1,
+  delay: 0.3,
+});
 
 //=========== ASYLUM ===================================
 
 gsap.set(".asylum h2", { opacity: 0, x: -50, borderBottom: 0 });
-gsap.to(".asylum h2", { duration: 1, x: 0, y: 0, opacity: 1, delay: .1 });
+gsap.to(".asylum h2", { duration: 1, x: 0, y: 0, opacity: 1, delay: 0.1 });
 
 gsap.set(".asylum img.face", { opacity: 0, x: -50, borderBottom: 0 });
-gsap.to(".asylum img.face", { duration: 1, x: 0, y: 0, opacity: 1, delay: .1 });
-
-
+gsap.to(".asylum img.face", {duration: 1, x: 0, y: 0, opacity: 1, delay: 0.1,});
 
 // ========== PROBLEMS & ACTIONS  ======================
-
 
 gsap.set($(".card-back"), { rotationY: -180 });
 
 $.each($(".card-container"), function (i, element) {
-
   var frontCard = $(this).children(".card-front"),
     backCard = $(this).children(".card-back"),
-    tlcardShow = new TimelineMax({ paused: true });
+    tlcardShow = gsap.timeline({ paused: true });
 
   tlcardShow
-    .to(frontCard, 1, { rotationY: 180 })
-    .to(backCard, 1, { rotationY: 0 }, 0)
-    ;
+    .to(frontCard, { duration: 1, rotationY: 180 })
+    .to(backCard, { duration: 1, rotationY: 0 }, 0);
   //.to(element, .5, {z:50},0)
   //.to(element, .5, {z:0},.5);
 
@@ -143,51 +159,42 @@ $.each($(".card-container"), function (i, element) {
   $(this).click(function () {
     if (forward) {
       this.animation.reverse();
-    }
-    else {
+    } else {
       this.animation.play();
     }
 
     forward = !forward;
-
   });
-
 });
 
+//Problem Read More Cards
+// //hide until read more btn clicked
+gsap.set(".card-full-3", { opacity: 0, scaleX: 0, scaleY: 0, display: "none" });
 
-
-//Problem Read More cards
-
-//Problem 3 read more cards
-//hide until read more btn clicked
-gsap.set([".card-full-3"], { opacity: 0, scaleX: 0, scaleY: 0, display: "none" });
-
-/*card-full show*/
+//card-full show
 var tlcardFull3 = new gsap.timeline({ paused: true });
+tlcardFull3.to(".card-full-3", {scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: 0.5, display: "flex",});
 
 $(".card-full-btn-3").on("click", function () {
   tlcardFull3.play();
 });
 
-tlcardFull3.to('.card-full-3', { scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: .5, display: "flex" });
-
 $(".card-full-close-btn-3").on("click", function () {
   tlcardFull3.reverse();
 });
 
-
 //Problem 5 read more cards
 //hide until read more btn clicked
-gsap.set([".card-full-5"], { opacity: 0, scaleX: 0, scaleY: 0, display: "none" });
+gsap.set(".card-full-5", { opacity: 0, scaleX: 0, scaleY: 0, display: "none" });
 
-/*card-full show*/
+//card-full show
 var tlcardFull5 = new gsap.timeline({ paused: true });
 
 $(".card-full-btn-5").on("click", function () {
   tlcardFull5.play();
 });
 
-tlcardFull5.to('.card-full-5', { scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: .5, display: "flex" });
+tlcardFull5.to(".card-full-5", {scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: 0.5, display: "flex",});
 
 $(".card-full-close-btn-5").on("click", function () {
   tlcardFull5.reverse();
@@ -195,21 +202,20 @@ $(".card-full-close-btn-5").on("click", function () {
 
 //Problem 7 read more cards
 //hide until read more btn clicked
-gsap.set([".card-full-7"], { opacity: 0, scaleX: 0, scaleY: 0, display: "none" });
+gsap.set(".card-full-7", { opacity: 0, scaleX: 0, scaleY: 0, display: "none" });
 
-/*card-full show*/
+//card-full show
 var tlcardFull7 = new gsap.timeline({ paused: true });
 
 $(".card-full-btn-7").on("click", function () {
   tlcardFull7.play();
 });
 
-tlcardFull7.to('.card-full-7', { scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: .5, display: "flex" });
+tlcardFull7.to(".card-full-7", {scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: 0.5, display: "flex",});
 
 $(".card-full-close-btn-7").on("click", function () {
   tlcardFull7.reverse();
 });
-
 
 //=========== STAR CARDS ==================
 
@@ -217,7 +223,6 @@ $(".card-full-close-btn-7").on("click", function () {
 gsap.set([".card"], { opacity: 0, scaleX: 0, scaleY: 0, display: "none" });
 
 ///////////////////////////////////////////////////////
-
 
 /*card-1 show*/
 
@@ -227,7 +232,14 @@ $(".card-btn-1").on("click", function () {
   tlcards1.play();
 });
 
-tlcards1.to('.card-1', { scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: .5, display: "block" });
+tlcards1.to(".card-1", {
+  scaleX: 1,
+  scaleY: 1,
+  transformOrigin: "center center",
+  opacity: 1,
+  duration: 0.5,
+  display: "block",
+});
 
 /*close card-1*/
 var tlcardsClose1 = new gsap.timeline({ paused: true });
@@ -243,7 +255,7 @@ $(".card-btn-2").on("click", function () {
   tlcards2.play();
 });
 
-tlcards2.to('.card-2', { scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: .5, display: "block" });
+tlcards2.to(".card-2", {scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: 0.5, display: "block",});
 
 var tlcardsClose2 = new gsap.timeline({ paused: true });
 
@@ -258,7 +270,14 @@ $(".card-btn-3").on("click", function () {
   tlcards3.play();
 });
 
-tlcards3.to('.card-3', { scaleX: 1, scaleY: 1, transformOrigin: "center center", opacity: 1, duration: .5, display: "block" });
+tlcards3.to(".card-3", {
+  scaleX: 1,
+  scaleY: 1,
+  transformOrigin: "center center",
+  opacity: 1,
+  duration: 0.5,
+  display: "block",
+});
 
 var tlcardsClose3 = new gsap.timeline({ paused: true });
 
@@ -268,15 +287,14 @@ $(".card-close-btn").on("click", function () {
 
 ///////////////////////////////////////////////////////
 
-
-//mouseover star cards 
-gsap.set(".card-btn", { transformOrigin: '100% 100%' });
+//mouseover star cards
+gsap.set(".card-btn", { transformOrigin: "100% 100%" });
 $(".card-btn").hover(cardOver, cardOut);
 
 function cardOver() {
-  gsap.to(this, .2, { rotation: '+=3' });
+  gsap.to(this, 0.2, { rotation: "+=3" });
 }
 
 function cardOut() {
-  gsap.to(this, .2, { rotation: '0' });
+  gsap.to(this, 0.2, { rotation: "0" });
 }
